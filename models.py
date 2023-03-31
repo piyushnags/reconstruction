05b4e396ad13ddf758b947cd723e784f337a5095
Piyush <piyushnags@gmail.com>
2023-03-31 13:42:00 -0400

    more fixes for noise addition in foward

--- models.py
+++ models.py
@@ -135,7 +135,7 @@ class Autoencoder(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         x = self.encoder(x)
         if self.noisy:
-            noise = (torch.randn(x.size(), requires_grad=True)*0.03 + 0.03)
+            noise = nn.Parameter(torch.randn(x.size())*0.03 + 0.03)
             x += noise
         x = self.decoder(x)
         return x
