793eae2a352fe6a0dfe77c6b6e39be3202f9a5e7
Piyush <piyushnags@gmail.com>
2023-04-13 11:47:48 -0400

    temp vis

--- utils.py
+++ utils.py
@@ -76,10 +76,10 @@ class AutoDataset(Dataset):
         self.transforms = transforms
         self.preprocess = T.Compose([
             T.Resize((320,320)),
-            T.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225],
-            )
+            # T.Normalize(
+            #     mean=[0.485, 0.456, 0.406],
+            #     std=[0.229, 0.224, 0.225],
+            # )
         ])
 
 
